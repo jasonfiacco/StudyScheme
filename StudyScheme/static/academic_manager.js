7dfb0c2bd6536af5a51d30acb81789a02b4b5788
Yifeng Ding <yding12321@gmail.com>
2017-07-19 12:11:46 -0400

    added various useful conversions

--- StudyScheme/static/academic_manager.js
+++ StudyScheme/static/academic_manager.js
@@ -1,4 +1,5 @@
-var values = {
+//Structure to keep track of values in editor
+var editorValues = {
   "majors" : {
 
   }, 
@@ -7,12 +8,75 @@ var values = {
   }
 };
 
-function creditsCompleted() {
-return 0;
+var GRADE_CONVERSIONS = {
+   0 : 'F',
+   2 : 'D-',
+   3 : 'D',
+   4 : 'D+',
+   5 : 'C-',
+   6 : 'C',
+   7 : 'C+',
+   8 : 'B-',
+   9 : 'B',
+  10 : 'B+',
+  11 : 'A-',
+  12 : 'A',
+  13 : 'A+'
+};
+
+/**
+* Calculates the grade from the current score
+* Note: uses the fixed calculations for now, assumes max is 4.0
+* @return double represeting GPA
+**/
+function getGPA(score) {
+  return min(score / 3.0, 4.0);
 }
 
+/**
+* Gets the total number of credits completed
+* @return integer representing the total number of credits completed
+**/
+function creditsCompleted() {
+  var courses = editorValues["courses"];
+  var credits = 0;
+  for (var course : courses) {
+    credits += courses[course]["credits"];
+  }
+  return credits;
+}
+
+/**
+* Calculates the number of credits completed in the major
+* @param majorID[integer]
+* @return integer representing the number of credits completed
+**/
 function majorCreditsCompleted(majorID) {
-  return 0;
+  var majors = editorValues["majors"];
+  if (!(majorID in majors)) {
+    console.log("Major " + majorID + " does not exist");
+    return 0;
+  }
+  major = majors[majorID];
+  credits = 0;
+  for (var courseID : major["courses"]) {
+    credits += getCourseCredits(courseID);
+  }
+  return credits;
+}
+
+/**
+* Gets the amount of credits a course is worth
+* @param courseID[integer]
+* @return integer representing the number of credits the course is worth
+**/
+function getCourseCredits(courseID) {
+  var courses = editorValues["courses"];
+  if (!(courseID in courses)) {
+    console.log("Course " + courseID + " does not exist");
+    return 0;
+  }
+  return courses[courseID]["credits"];
 }
 
 function actualAverageGPA() {
@@ -37,13 +101,14 @@ function majorGPA(majorID) {
 * @return boolean indicating success or failure
 **/
 function addMajor(id, major, creditsNeeded) {
-  var majors = values["majors"]
+  var majors = editorValues["majors"]
 
   if (renderMajor(id, major, creditsNeeded)) {
     //add to dictionary
     majorDict = {};
     majorDict["name"] = major;
     majorDict["credits"] = creditsNeeded;
+    majorDict["courses"] = [];
     majors[id] = majorDict;
     return true;
   }
@@ -56,7 +121,7 @@ function addMajor(id, major, creditsNeeded) {
 * @return boolean indicating success or failure
 **/
 function renderMajor(id, major, creditsNeeded) {
-  var majors = values["majors"]
+  var majors = editorValues["majors"]
   if (id in majors) {
     console.log("Major " + id + " already exists");
     return false;
@@ -68,17 +133,21 @@ function renderMajor(id, major, creditsNeeded) {
   var majorData = document.createElement("td");
   majorData.innerHTML = major;
 
+  //render input for credits needed
   var creditsData = document.createElement("td");
   var creditsEntry = document.createElement("input");
   creditsEntry.type = "text";
   creditsEntry.value = creditsNeeded;
   creditsEntry.id = "creditsNeeded-" + id;
+  creditsEntry.className += "creditsNeeded"
   creditsData.appendChild(creditsEntry);
 
+  //render remaining credits needed 
   var creditsRemainingData = document.createElement("td");
   creditsRemainingData.id = "creditsRemaining-" + id;
   creditsRemainingData.innerHTML = creditsNeeded;
 
+  //render GPA slot
   var majorGPAData = document.createElement("td");
   majorGPAData.id = "majorGPA-" + id;
   majorGPAData.innerHTML = majorGPA(id).toFixed(2);
