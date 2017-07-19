702974fdca21a8cea26c9f7ca496bf1a92b4b1c7
Yifeng Ding <yding12321@gmail.com>
2017-07-18 21:29:57 -0400

    added more dummy functions

--- StudyScheme/static/academic_manager.js
+++ StudyScheme/static/academic_manager.js
@@ -1,8 +1,18 @@
 function creditsCompleted() {
   return 0;
 }
+function actualAverageGPA() {
+  return 0;
+}
+function anticipatedAverageGPA() {
+  return 0;
+}
+function highestPossibleGPA() {
+  return 0;
+}
+
 $(function() {
   $("#credits_needed").change(function(){
-    $("#credits_remaining").text(parseInt($("#credits_needed").val() - creditsCompleted());
+    $("#credits_remaining").text(parseInt($("#credits_needed").val()) - creditsCompleted());
   });
 });
