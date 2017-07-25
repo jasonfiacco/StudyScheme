23b8d5fd41d3f7eaf248f6903bd48a8a99740bd6
Jason Fiacco <jason.fiacco@yale.edu>
2017-07-25 10:55:13 -0400

    Updated header html

--- StudyScheme/views.py
+++ StudyScheme/views.py
@@ -9,7 +9,7 @@ from flask_login import LoginManager, login_user, logout_user, current_user, log
 db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login.html"
+login_manager.login_view = "login"
 
 
 @app.route('/')
